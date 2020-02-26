1859013e62f3e742220c0a7d649f272242a1e2fb
tyroPatel <tyresecook25@yahoo.com>
2020-02-25 20:35:46 -0600

    [dev]: Updated from urllib => requests

--- code_retriever.py
+++ code_retriever.py
@@ -1,6 +1,7 @@
 # Code Retriever
 from bs4 import BeautifulSoup
 import urllib.request
+import requests
 
 def is_obdii(code):
 	requirements = {
@@ -20,25 +21,19 @@ def is_obdii(code):
 	return is_code
 			
 def retrieve_info(code):
-	info = {}
 	if is_obdii(code):
-		site_link = "https://www.obd-codes.com/"
-		full_site_link = site_link + code
-		headers = {}
-		headers['User-Agent'] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0"
-		request = urllib.request.Request(full_site_link, headers=headers)
-
-		with urllib.request.urlopen(request) as response:
-			html = response.read()
-
-		soup = BeautifulSoup(html,'html.parser')
-		#name = soup.find('title').string
-
-		#print(name)
-		
+		full_site_link = "https://www.obd-codes.com/" + code
+		response = requests.get(full_site_link)
+		response_text = response.text
+		soup = BeautifulSoup(response_text,'html.parser')
+		info = {
+			'name':soup.find('h1').string
+		}
+		print(info['name'])
 
 def main():
-	retrieve_info("p0231")
+	code = input("Enter Code: ")
+	retrieve_info(code)
 
 if __name__ == '__main__':
 	main()
